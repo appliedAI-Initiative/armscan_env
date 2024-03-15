380fccf45a9541adf24159574ad360f9a1b5f79a
carlocagnetta <c.cagnetta@appliedai.de>
2024-03-15 17:47:42 +0100

    Create clustering dataclasses

--- src/armscan_env/clustering.py
+++ src/armscan_env/clustering.py
@@ -1,75 +1,80 @@
+from dataclasses import dataclass
 from typing import Any
 
 import numpy as np
 from numpy import dtype, ndarray
 from scipy.ndimage import label
 from sklearn.cluster import DBSCAN
+import logging
+
+log = logging.getLogger(__name__)
 
 
-def find_clusters(tissue_value: int, slice: np.ndarray) -> list[dict]:
+@dataclass(kw_only=True)
+class DataCluster:
+    cluster: list[tuple[float, float]]
+    center: tuple[float, float]
+
+
+@dataclass(kw_only=True)
+class TissueClusters:
+    bones: list[DataCluster]
+    tendons: list[DataCluster]
+    ulnar: list[DataCluster]
+
+
+def find_clusters(tissue_value: int, slice: np.ndarray) -> list[DataCluster]:
     """Find clusters of a given tissue in a slice
     :param tissue_value: value of the tissue to cluster
     :param slice: image slice to cluster
     :return: list of clusters and their centers.
     """
-    # Create a binary mask based on the threshold
     binary_mask = slice == tissue_value
-
-    # Check if there are tissues with given label
     if np.all(binary_mask is False):
-        print("No tissues to cluster. Please set values using set_values method.")
+        log.debug("No tissues to cluster. Please set values using set_values method.")
         return []
 
-    # Label connected components in the binary mask
     labeled_array, num_clusters = label(binary_mask)
-
-    # Extract clusters and their centers
-    cluster_data = []
-
+    cluster_list = []
     for cluster_label in range(num_clusters):
         cluster_indices = np.where(labeled_array == cluster_label + 1)
-        # Calculate the center of the cluster
         center_x = np.mean(cluster_indices[0])
         center_y = np.mean(cluster_indices[1])
         center = (center_x, center_y)
 
-        # Save both the cluster and center under the same key
-        cluster_data.append(
-            {
-                "cluster": np.array(
-                    list(zip(cluster_indices[0], cluster_indices[1], strict=False)),
-                ),
-                "center": center,
-            },
+        cluster_list.append(
+            DataCluster(
+                cluster=list(zip(cluster_indices[0], cluster_indices[1], strict=True)),
+                center=center,
+            )
         )
+    return cluster_list
 
-    return cluster_data
 
-
-def cluster_iter(tissues: dict, slice: np.ndarray) -> dict:
+def cluster_iter(tissues: dict, slice: np.ndarray) -> TissueClusters:
     """Find clusters of all tissues in a slice
     :param tissues: dictionary of tissues and their values
     :param slice: image slice to cluster
     :return: dictionary of tissues and their clusters.
     """
-    # store clusters of tissues in a dict
-    tissues_clusters = {}
+    tissues_clusters = TissueClusters(
+        bones=find_clusters(tissues["bones"], slice),
+        tendons=find_clusters(tissues["tendons"], slice),
+        ulnar=find_clusters(tissues["ulnar"], slice),
+    )
+    log.debug(f"Found {len(tissues_clusters.bones)} bones clusters")
+    log.debug(f"Found {len(tissues_clusters.tendons)} tendons clusters")
+    log.debug(f"Found {len(tissues_clusters.ulnar)} ulnar clusters")
 
-    for tissue in tissues:
-        print(f"Finding {tissue} clusters, with value {tissues[tissue]}:")
-        tissues_clusters[tissue] = find_clusters(tissues[tissue], slice)
-
-        print(f"Found {len(tissues_clusters[tissue])} clusters\n")
-    print("---------------------------------------\n")
     return tissues_clusters
 
 
 def find_DBSCAN_clusters(
-    tissue_value: int,
-    slice: np.ndarray,
-    eps: float,
-    min_samples: int,
-) -> list[Any] | list[dict[str, ndarray[Any, dtype[Any]] | Any]]:
+        tissue_value: int,
+        slice: np.ndarray,
+        eps: float,
+        min_samples: int,
+) -> list[DataCluster]:
     """Find clusters of a given tissue in a slice using DBSCAN
     :param tissue_value: value of the tissue to cluster
     :param slice: image slice to cluster
@@ -77,40 +82,36 @@ def find_DBSCAN_clusters(
     :param min_samples: The number of samples (or total weight) in a neighborhood for a point to be considered as a core point.
     :return: list of clusters and their centers.
     """
-    # binary filter for the tissue_value
     binary_mask = slice == tissue_value
-
-    # Check if there are tissues with given tissue_value
     if np.all(binary_mask == 0):
-        print("No tissues to cluster with given value.")
+        log.debug("No tissues to cluster with given value.")
         return []
 
-    # find label positions, upon which clustering wil be defined
-    label_positions = np.array(list(zip(*np.where(binary_mask), strict=True)))
-    # define clusterer
+    label_positions = list(zip(*np.where(binary_mask), strict=True))
     clusterer = DBSCAN(eps=eps, min_samples=min_samples)
-
-    # find cluster prediction
     clusters = clusterer.fit_predict(label_positions)
     n_clusters = (
-        len(np.unique(clusters)) - 1
+            len(np.unique(clusters)) - 1
     )  # noise cluster has label -1, we don't take it into account
-    print(f"Found {n_clusters} clusters")
+    log.debug(f"Found {n_clusters} clusters")
 
-    # Extract clusters and their centers
-    cluster_data = []
-
+    cluster_list = []
     for cluster in range(n_clusters):
         label_to_pos_array = label_positions[clusters == cluster]  # get positions of each cluster
         cluster_centers = np.mean(label_to_pos_array, axis=0)  # mean of each column
-        # Save both the cluster and center under the same key
-        cluster_data.append({"cluster": label_to_pos_array, "center": cluster_centers})
 
-    return cluster_data
+        cluster_list.append(
+            DataCluster(
+                cluster=label_to_pos_array,
+                center=cluster_centers
+            )
+        )
+
+    return cluster_list
 
 
-# TODO: set different parameters for each tissue
-def DBSCAN_cluster_iter(tissues: dict, slice: np.ndarray, eps: float, min_samples: int) -> dict:
+# TODO: set different parameters for each tissue from config
+def DBSCAN_cluster_iter(tissues: dict, slice: np.ndarray, eps: float, min_samples: int) -> TissueClusters:
     """Find clusters of all tissues in a slice using DBSCAN
     :param tissues: dictionary of tissues and their values
     :param slice: image slice to cluster
@@ -118,16 +119,15 @@ def DBSCAN_cluster_iter(tissues: dict, slice: np.ndarray, eps: float, min_sample
     :param min_samples: The number of samples (or total weight) in a neighborhood for a point to be considered as a core point.
     :return: dictionary of tissues and their clusters.
     """
-    # store clusters of tissues in a dict
-    tissues_clusters = {}
 
-    for tissue in tissues:
-        print(f"Finding {tissue} clusters, with value {tissues[tissue]}:")
-        # find clusters for each tissue
-        tissues_clusters[tissue] = find_DBSCAN_clusters(tissues[tissue], slice, eps, min_samples)
+    bones = find_DBSCAN_clusters(tissues["bones"], slice, eps=4.1, min_samples=46)
+    tendons = find_DBSCAN_clusters(tissues["tendons"], slice, eps=4.1, min_samples=46)
+    ulnar = find_DBSCAN_clusters(tissues["ulnar"], slice, eps=2.5, min_samples=18)
 
-        # print the identified clusters and their centers
-        for index, data in enumerate(tissues_clusters[tissue]):
-            print(f"Center of {tissue} cluster {index}: {data['center']}")
-    print("---------------------------------------\n")
+    tissues_clusters = TissueClusters(
+        bones=bones,
+        tendons=tendons,
+        ulnar=ulnar
+    )
+
     return tissues_clusters

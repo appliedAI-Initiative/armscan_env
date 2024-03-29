c61baf0eabc04ace9c029c3176faea1cc3f4492e
carlocagnetta <c.cagnetta@appliedai.de>
2024-03-29 15:33:43 +0100

    * pre-commit clean-nbs * translation bounds on array size * robust render modes

--- src/armscan_env/envs/labelmaps_navigation.py
+++ src/armscan_env/envs/labelmaps_navigation.py
@@ -1,8 +1,9 @@
 import logging
 from abc import ABC
 from collections.abc import Sequence
+from copy import copy
 from dataclasses import dataclass
-from typing import Any, ClassVar, Self
+from typing import Any, ClassVar, Literal, Self
 
 import gymnasium as gym
 import numpy as np
@@ -20,8 +21,12 @@ from armscan_env.envs.rewards import anatomy_based_rwd
 from armscan_env.slicing import slice_volume
 from armscan_env.util.img_processing import crop_center
 from armscan_env.util.visualizations import show_clusters
+from celluloid import Camera
 from gymnasium.core import ObsType as TObs
+from IPython.core.display import HTML
 from matplotlib import pyplot as plt
+from matplotlib.animation import ArtistAnimation
+from matplotlib.axes import Axes
 from matplotlib.figure import Figure
 
 log = logging.getLogger(__name__)
@@ -33,18 +38,21 @@ class ManipulatorAction:
     """Array of shape (2,) representing two angles in degrees (z_rot, x_rot). The angles will take values between
     -180 and 180 deg."""
     translation: np.ndarray
-    """Array of shape (2,) representing two translations (x_trans, y_trans). TODO: extend description."""
+    """Array of shape (2,) representing two translations (x_trans, y_trans) in mm."""
 
     def to_normalized_array(
         self,
         angle_bounds: tuple[float, float],
-        translation_bounds: tuple[float, float],
+        translation_bounds: tuple[float, float] | None,
     ) -> np.ndarray:
         """Converts the action to a 1D array. If angle_bounds is not None, the angles will be normalized to the range
         [-1, 1] using the provided bounds.
         """
+        if translation_bounds is None:
+            raise ValueError("Translation bounds must not be None,this should not happen.")
         rotation = self.rotation / angle_bounds
-        translation = self.translation / translation_bounds
+        # normalize translation to [-1, 1]: 0 -> -1, translation_bounds -> 1
+        translation = 2 * self.translation / translation_bounds - 1
 
         result = np.concatenate([rotation, translation])
 
@@ -60,8 +68,8 @@ class ManipulatorAction:
     def from_normalized_array(
         cls,
         action: np.ndarray,
-        angle_bounds: tuple[float, float] | None,
-        translation_bounds: tuple[float, float] | None,
+        angle_bounds: tuple[float, float],
+        translation_bounds: tuple[float, float],
     ) -> Self:
         """Converts a 1D array to a ManipulatorAction. If angle_bounds is not None, the angles will be unnormalized
         using the provided bounds.
@@ -74,7 +82,8 @@ class ManipulatorAction:
             )
 
         rotation = action[:2] * angle_bounds
-        translation = action[2:] * translation_bounds
+        # unnormalize translation: -1 -> 0, 1 -> translation_bounds
+        translation = (action[2:] + 1) / 2 * translation_bounds
         log.debug(f"Unnormalized action: {rotation=} deg, {translation=}")
 
         return cls(rotation=rotation, translation=translation)
@@ -136,7 +145,7 @@ class LabelmapEnvTerminationCriterion(TerminationCriterion["LabelmapEnv"], ABC):
 class LabelmapEnv(ModularEnv[LabelmapStateAction, np.ndarray, np.ndarray]):
     _INITIAL_POS_ROTATION = np.zeros(4)
 
-    metadata: ClassVar[dict] = {"render_modes": ["human", "rgb_array"], "render_fps": 10}
+    metadata: ClassVar[dict] = {"render_modes": ["plt", "animation", None], "render_fps": 10}
 
     def __init__(
         self,
@@ -146,8 +155,8 @@ class LabelmapEnv(ModularEnv[LabelmapStateAction, np.ndarray, np.ndarray]):
         termination_criterion: TerminationCriterion | None = None,
         max_episode_len: int | None = None,
         angle_bounds: tuple[float, float] = (180, 180),
-        translation_bounds: tuple[float, float] = (10, 10),
-        render_mode: str | None = None,
+        translation_bounds: tuple[float, float] | None = None,
+        render_mode: Literal["plt", "animation"] | None = None,
     ):
         """:param name2volume: mapping from labelmap names to volumes. One of these volumes will be selected at reset.
 
@@ -158,6 +167,11 @@ class LabelmapEnv(ModularEnv[LabelmapStateAction, np.ndarray, np.ndarray]):
         """
         if not name2volume:
             raise ValueError("name2volume must not be empty")
+        if render_mode not in self.metadata["render_modes"]:
+            raise ValueError(
+                f"Unknown {render_mode=}. Allowed values: {self.metadata['render_modes']}",
+            )
+
         observation = LabelmapSliceObservation(slice_shape, render_mode)
         super().__init__(reward_metric, observation, termination_criterion, max_episode_len)
         self.name2volume = name2volume
@@ -170,10 +184,10 @@ class LabelmapEnv(ModularEnv[LabelmapStateAction, np.ndarray, np.ndarray]):
         self.angle_bounds = angle_bounds
         self.translation_bounds = translation_bounds
 
-        assert render_mode is None or render_mode in self.metadata["render_modes"]
         self.render_mode = render_mode
-        self.fig: Figure | None = None
-        self.axes: tuple[plt.Axes, plt.Axes, plt.Axes] | None = None
+        self._fig: Figure | None = None
+        self._axes: tuple[plt.Axes, plt.Axes, plt.Axes, plt.Axes, plt.Axes] | None = None
+        self._camera: Camera | None = None
 
     def unnormalize_rotation_translation(self, action: np.ndarray) -> ManipulatorAction:
         """Unnormalizes an array with values in the range [-1, 1] to the original range that is
@@ -182,6 +196,8 @@ class LabelmapEnv(ModularEnv[LabelmapStateAction, np.ndarray, np.ndarray]):
         :param action: normalized action with values in the range [-1, 1]
         :return: unnormalized action that can be used with :func:`slice_volume`
         """
+        if self.translation_bounds is None:
+            raise ValueError("Translation bounds must not be None,this should not happen.")
         return ManipulatorAction.from_normalized_array(
             action,
             self.angle_bounds,
@@ -241,6 +257,7 @@ class LabelmapEnv(ModularEnv[LabelmapStateAction, np.ndarray, np.ndarray]):
         sampled_image_name = np.random.choice(list(self.name2volume.keys()))
         self._cur_labelmap_name = sampled_image_name
         self._cur_labelmap_volume = self.name2volume[sampled_image_name]
+        self.get_translation_bounds()
         # Alternatively, select a random slice
         initial_slice = self._get_initial_slice()
         return LabelmapStateAction(
@@ -250,99 +267,157 @@ class LabelmapEnv(ModularEnv[LabelmapStateAction, np.ndarray, np.ndarray]):
             optimal_labelmap=None,
         )
 
+    def compute_translation_bounds(self) -> None:
+        """Compute the translation bounds from the volume size."""
+        if self.cur_labelmap_volume is None:
+            raise RuntimeError("The labelmap volume must not be None, did you call reset?")
+        volume = self.cur_labelmap_volume
+        size = volume.GetSize()
+        spacing = volume.GetSpacing()
+        self.translation_bounds = size[0] * spacing[0], size[1] * spacing[1]
+
+    def get_translation_bounds(self) -> tuple[float, float] | None:
+        if self.translation_bounds is None:
+            self.compute_translation_bounds()
+        return self.translation_bounds
+
     def step(
         self,
         action: np.ndarray | ManipulatorAction,
     ) -> tuple[TObs, float, bool, bool, dict[str, Any]]:
         if isinstance(action, ManipulatorAction):
             action = action.to_normalized_array(self.angle_bounds, self.translation_bounds)
-        if self.render_mode == "human":
-            self.render()
         return super().step(action)
 
-    def reset(self, seed: int | None = DEFAULT_SEED, **kwargs: Any) -> tuple[TObs, dict[str, Any]]:
+    def reset(
+        self,
+        seed: int | None = DEFAULT_SEED,
+        reset_render: bool = True,
+        reset_translation_bounds: bool = True,
+        **kwargs: Any,
+    ) -> tuple[TObs, dict[str, Any]]:
+        if reset_render:
+            self.reset_render()
+        if reset_translation_bounds:
+            self.translation_bounds = None
         obs, info = super().reset(seed=seed, **kwargs)
-        if self.render_mode == "human":
-            self.render()
         return obs, info
 
-    def render(self) -> plt.Figure | None:
-        if self.fig is None or self.axes is None:
-            self._create_figure()
+    def render(self) -> plt.Figure | Camera | None:
+        match self.render_mode:
+            case "plt":
+                return self.get_cur_state_plot(create_new_figure=False)
+            case "animation":
+                camera = self.get_camera()
+                self.get_cur_state_plot(create_new_figure=False)
+                camera.snap()
+                return camera
+            case None:
+                gym.logger.warn(
+                    "You are calling render method without having specified any render mode. "
+                    "You can specify the render_mode at initialization, "
+                    f'e.g. gym.make("{self.spec.id}", render_mode="rgb_array")',
+                )
+                return None
+            case _:
+                raise RuntimeError(
+                    f"Unknown render mode: {self.render_mode}, this should not happen.",
+                )
 
-        if self.render_mode is None:
-            assert self.spec is not None
-            gym.logger.warn(
-                "You are calling render method without specifying any render mode. "
-                "You can specify the render_mode at initialization, "
-                f'e.g. gym.make("{self.spec.id}", render_mode="rgb_array")',
-            )
-            return None
+    def get_cur_state_plot(self, create_new_figure: bool = True) -> plt.Figure | None:
+        """Retrieve a figure visualizing the current state of the environment.
 
-        if self.render_mode == "human":
-            gym.logger.warn(
-                "Render mode 'human' has not been implemented yet."
-                "If you want to render the environment, please use 'rgb_array' mode.",
-            )
-            return None
-        else:  # mode in "rgb_array"
-            return self._plot_cur_state()
+        :param create_new_figure: if True, a new figure will be created. Otherwise, a single figure will be used
+            in subsequent calls to this method. False may be useful for animations.
+        """
+        fig, (ax1, ax2, ax3, ax4, ax5) = self.get_figure_axes()  # type: ignore
+        if create_new_figure:
+            fig, ax1, ax2, ax3, ax4, ax5 = copy((fig, ax1, ax2, ax3, ax4, ax5))
 
-    def _create_figure(self) -> None:
-        self.fig = plt.figure(constrained_layout=True, figsize=(9, 6))
-        gs = self.fig.add_gridspec(nrows=2, ncols=2)
-        # Add subplots
-        ax2 = self.fig.add_subplot(gs[0, 1])
-        ax3 = self.fig.add_subplot(gs[1, 1])
-        ax1 = self.fig.add_subplot(gs[:, 0])
-        self.axes = (ax1, ax2, ax3)
+        if self.cur_labelmap_volume is None:
+            raise RuntimeError("The labelmap volume must not be None, did you call reset?")
 
-    def _plot_cur_state(self) -> plt.Figure | None:
-        """Plot the current state of the environment."""
-        if self.fig is None and self.axes is None:
-            gym.logger.warn("No figure or axes have been created. Creating them now.")
-            self._create_figure()
-
-        fig = self.fig
-        assert self.axes is not None and len(self.axes) == 3
-        ax1, ax2, ax3 = self.axes
-
-        assert self._cur_labelmap_volume is not None
-        volume = self._cur_labelmap_volume
+        volume = self.cur_labelmap_volume
         o = volume.GetOrigin()
-        img_array = sitk.GetArrayFromImage(volume)[40, :, :]
+        img_array = sitk.GetArrayFromImage(volume)
         action = self.unnormalize_rotation_translation(self.cur_state_action.action)
         translation = action.translation
         rotation = action.rotation
 
         # Subplot 1: Image with dashed line
-        ax1.imshow(img_array)
-        x_dash = np.arange(img_array.shape[1])
+        ax1.imshow(img_array[40, :, :])
+        x_dash = np.arange(img_array.shape[2])
         b = volume.TransformPhysicalPointToIndex([o[0], o[1] + translation[1], o[2]])[1]
         y_dash = np.tan(np.deg2rad(rotation[0])) * x_dash + b
-        y_dash = np.clip(y_dash, 0, img_array.shape[0] - 1)
-        ax1.set_title(f"Section {0}")
+        y_dash = np.clip(y_dash, 0, img_array.shape[1] - 1)
         ax1.plot(x_dash, y_dash, linestyle="--", color="red")
         ax1.set_title("Slice cut")
 
+        ax2.imshow(img_array[:, :, 265].T, aspect=0.24)
+        z_dash = np.arange(img_array.shape[0])
+        # y has x size and b value
+        y_dash_2 = np.tan(np.deg2rad(rotation[1])) * z_dash + b
+        y_dash_2 = np.clip(y_dash_2, 0, img_array.shape[1] - 1)
+        ax2.plot(z_dash, y_dash_2, linestyle="--", color="red")
+
         # ACTION
-        sliced_volume = slice_volume(
-            volume=volume,
-            z_rotation=rotation[0],
-            x_rotation=rotation[1],
-            x_trans=translation[0],
-            y_trans=translation[1],
+        sliced_img = self.cur_state_action.labels_2d_slice
+        ax3.imshow(sliced_img, origin="lower", aspect=6)
+
+        txt = (
+            "Slice taken at position:\n"
+            f"y: {translation[1]:.2f} mm,\n"
+            f"x: {translation[0]:.2f} mm,\n"
+            f"rot_z: {rotation[0]:.2f} mm,\n"
+            f"rot_x: {rotation[1]:.2f} mm"
         )
-        sliced_img = sitk.GetArrayFromImage(sliced_volume)[:, 0, :]
-        ax2.imshow(sliced_img, origin="lower", aspect=6)
+        ax4.text(0.5, 0.5, txt, ha="center", va="center")
+        ax4.axis("off")
 
         # OBSERVATION
         clusters = TissueClusters.from_labelmap_slice(self.cur_state_action.labels_2d_slice)
-        show_clusters(clusters, sliced_img, ax3)
+        show_clusters(clusters, sliced_img, ax5)
 
         # REWARD
-        loss = anatomy_based_rwd(clusters)
-        ax3.text(0, 0, f"Loss: {loss:.2f}", fontsize=12, color="red")
+        ax5.text(0, 0, f"Reward: {self.cur_reward:.2f}", fontsize=12, color="red")
 
         plt.close()
         return fig
+
+    def _create_figure_axis(self) -> None:
+        fig = plt.figure(constrained_layout=True, figsize=(9, 6))
+        gs = fig.add_gridspec(nrows=3, ncols=3)
+
+        ax1 = fig.add_subplot(gs[:, 0])
+        ax2 = fig.add_subplot(gs[:, 1])
+        ax3 = fig.add_subplot(gs[0, 2])
+        ax4 = fig.add_subplot(gs[1, 2])
+        ax5 = fig.add_subplot(gs[2, 2])
+
+        self._fig = fig
+        self._axes = (ax1, ax2, ax3, ax4, ax5)
+
+    def get_figure_axes(self) -> tuple[Figure | None, tuple[Axes, Axes, Axes, Axes, Axes] | None]:
+        if self._fig is None or self._axes is None:
+            self._create_figure_axis()
+        if self._fig is None or self._axes is None:
+            raise RuntimeError("Could not create figure and axes.")
+        return self._fig, self._axes
+
+    def get_camera(self) -> Camera:
+        if self._camera is None:
+            fig, ax = self.get_figure_axes()
+            self._camera = Camera(fig)
+        return self._camera
+
+    def get_cur_animation(self) -> ArtistAnimation:
+        return self.get_camera().animate()
+
+    def get_cur_animation_as_html(self) -> HTML:
+        anim = self.get_cur_animation()
+        return HTML(anim.to_jshtml())
+
+    def reset_render(self) -> None:
+        self._camera = None
+        self._fig = None
+        self._axes = None

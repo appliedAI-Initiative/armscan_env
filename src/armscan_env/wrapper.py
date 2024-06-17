15604a8eb5cd06f159a88291c8166cb5b4eec2c2
Michael Panchenko <m.panchenko@appliedai.de>
2024-06-17 17:53:51 +0200

    Wrapper: don't use gym's FrameStackObservation, instead use own patch

--- src/armscan_env/wrapper.py
+++ src/armscan_env/wrapper.py
@@ -2,7 +2,9 @@
 # https://github.com/openai/baselines/blob/master/baselines/common/atari_wrappers.py
 import logging
 from abc import ABC, abstractmethod
-from typing import Any, Literal, SupportsFloat, TypeVar, cast
+from collections import deque
+from copy import deepcopy
+from typing import Any, Final, Literal, SupportsFloat, TypeVar, cast
 
 import numpy as np
 import SimpleITK as sitk
@@ -15,9 +17,11 @@ from armscan_env.envs.observations import MultiBoxSpace
 from armscan_env.envs.rewards import LabelmapClusteringBasedReward
 from armscan_env.envs.state_action import LabelmapStateAction
 
+import gymnasium as gym
 from gymnasium.core import Env, Wrapper
 from gymnasium.spaces import Dict as DictSpace
-from gymnasium.wrappers import FrameStackObservation
+from gymnasium.vector.utils import batch_space, concatenate, create_empty_array
+from gymnasium.wrappers.utils import create_zero_array
 from tianshou.highlevel.env import (
     EnvFactory,
     EnvMode,
@@ -30,16 +34,106 @@ ActType = TypeVar("ActType")
 log = logging.getLogger(__name__)
 
 
-class PatchedFrameStackObservation(FrameStackObservation):
-    def __init__(self, env: Env[ObsType, ActType], n_stack: int):
-        super().__init__(env, n_stack)
-        if isinstance(self.observation_space, DictSpace):  # type: ignore
-            self.observation_space = MultiBoxSpace(self.observation_space)  # type: ignore
+class PatchedFrameStackObservation(Wrapper):
+    def __init__(
+        self,
+        env: gym.core.Env[ObsType, ActType],
+        stack_size: int,
+        *,
+        padding_type: str | ObsType = "reset",
+    ):
+        """Had to copy-paste and adjust.
+
+        The inheriting from `RecordConstructorArgs` in original FrameStack is not possible together with
+        overridden getattr, which we however need in order to not become crazy.
+        So there is no way of fixing this after inheriting from original FrameStackObservation, wherefore
+        we copy-paste the whole class and adjust it.
+
+        Adjustments:
+            1. reset takes **kwargs
+            2. render takes **kwargs
+            3. __getattr__ is overridden to pass the attribute to the wrapped environment (like in pre-1.0 wrappers)
+            4. No inheritance from RecordConstructorArgs
+            5. Observation space is converted to MultiBoxSpace if it is a DictSpace
+        """
+        super().__init__(env)
+
+        if not np.issubdtype(type(stack_size), np.integer):
+            raise TypeError(
+                f"The stack_size is expected to be an integer, actual type: {type(stack_size)}",
+            )
+        if not stack_size > 1:
+            raise ValueError(
+                f"The stack_size needs to be greater than one, actual value: {stack_size}",
+            )
+        if isinstance(padding_type, str) and (padding_type == "reset" or padding_type == "zero"):
+            self.padding_value: ObsType = create_zero_array(env.observation_space)
+        elif padding_type in env.observation_space:
+            self.padding_value = padding_type # type: ignore
+            padding_type = "_custom"
+        else:
+            if isinstance(padding_type, str):
+                raise ValueError(  # we are guessing that the user just entered the "reset" or "zero" wrong
+                    f"Unexpected `padding_type`, expected 'reset', 'zero' or a custom observation space, actual value: {padding_type!r}",
+                )
+            else:  # noqa: RET506
+                raise ValueError(
+                    f"Unexpected `padding_type`, expected 'reset', 'zero' or a custom observation space, actual value: {padding_type!r} not an instance of env observation ({env.observation_space})",
+                )
+
+        self.observation_space = batch_space(env.observation_space, n=stack_size)
+        self.stack_size: Final[int] = stack_size
+        self.padding_type: Final[str] = padding_type
+
+        self.obs_queue = deque(
+            [self.padding_value for _ in range(self.stack_size)],
+            maxlen=self.stack_size,
+        )
+        self.stacked_obs = create_empty_array(env.observation_space, n=self.stack_size)
+
+        if isinstance(self.observation_space, DictSpace):
+            self.observation_space = MultiBoxSpace(self.observation_space)
+
+    def step(
+        self,
+        action: ActType,
+    ) -> tuple[ObsType, SupportsFloat, bool, bool, dict[str, Any]]:
+        obs, reward, terminated, truncated, info = self.env.step(action)
+        self.obs_queue.append(obs)
+
+        updated_obs = deepcopy(
+            concatenate(self.env.observation_space, self.obs_queue, self.stacked_obs),
+        )
+        return updated_obs, reward, terminated, truncated, info
+
+    def reset(
+        self,
+        **kwargs: Any
+    ) -> tuple[ObsType, dict[str, Any]]:
+        obs, info = self.env.reset(**kwargs)
+
+        if self.padding_type == "reset":
+            self.padding_value = obs
+        for _ in range(self.stack_size - 1):
+            self.obs_queue.append(self.padding_value)
+        self.obs_queue.append(obs)
+
+        updated_obs = deepcopy(
+            concatenate(self.env.observation_space, self.obs_queue, self.stacked_obs),
+        )
+        return updated_obs, info
+
+    def render(self, **kwargs: Any) -> Any:
+        return self.env.render(**kwargs)
+
+    # Like in PatchedWrapper
+    def __getattr__(self, item: str) -> Any:
+        return getattr(self.env, item)
 
 
 class ArmscanEnvFactory(EnvFactory):
     """:param name2volume: the gymnasium task/environment identifier
-    :param observation: the observation space to use
+    :param observation: the observation to use
     :param reward_metric: the reward metric to use
     :param termination_criterion: the termination criterion to use
     :param slice_shape: the shape of the slice
@@ -74,7 +168,6 @@ class ArmscanEnvFactory(EnvFactory):
         seed: int | None = None,
         n_stack: int = 1,
         project_actions_to: Literal["x", "y", "xy"] | None = None,
-        remove_rotation_actions: bool = False,
         **make_kwargs: Any,
     ) -> None:
         super().__init__(venv_type)
@@ -94,7 +187,6 @@ class ArmscanEnvFactory(EnvFactory):
         self.seed = seed
         self.n_stack = n_stack
         self.project_actions_to = project_actions_to
-        self.remove_rotation_actions = remove_rotation_actions
         self.make_kwargs = make_kwargs
 
     def _create_kwargs(self) -> dict:
